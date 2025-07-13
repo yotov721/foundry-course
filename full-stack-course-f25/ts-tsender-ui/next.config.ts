5c831ad22c31975a6f31cbe079e508cef128c27c
Yoto Yotov <yotov721@gmail.com>
2025-07-13 15:31:56 +0300

    Update next config

--- full-stack-course-f25/ts-tsender-ui/next.config.ts
+++ full-stack-course-f25/ts-tsender-ui/next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // build as static site
+  distDir: "out", // compile location
+  images: {
+    unoptimized: true,
+  },
+  basePath: "",
+  assetPrefix: "",
+  trailingSlash: true
 };
 
 export default nextConfig;
+
+// pnpm next build -> build as static site for deployment 
